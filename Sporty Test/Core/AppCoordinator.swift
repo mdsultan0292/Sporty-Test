ad7646aaa77a6c4a999194ec98ea1ec39c8e08c4
Sporty <sporty@example.com>
2025-05-20 16:18:00 +0100

    Initial commit

--- Sporty Test/Core/AppCoordinator.swift
+++ Sporty Test/Core/AppCoordinator.swift
@@ -5,6 +5,7 @@ import UIKit
 @MainActor
 final class AppCoordinator {
     private let window: UIWindow
+<<<<<<< HEAD
     private let repositoryService: GitHubRepositoryService
     private let mockLiveServer: MockLiveServer
     
@@ -28,6 +29,24 @@ final class AppCoordinator {
             }
         )
         window.rootViewController = UINavigationController(rootViewController: listViewController)
+=======
+    private let gitHubAPI: GitHubAPI
+    private let mockLiveServer: MockLiveServer
+
+    init(window: UIWindow) {
+        self.window = window
+        gitHubAPI = GitHubAPI(authorisationToken: nil)
+        mockLiveServer = MockLiveServer()
+    }
+
+    func start() {
+        window.rootViewController = UINavigationController(
+            rootViewController: RepositoriesViewController(
+                gitHubAPI: gitHubAPI,
+                mockLiveServer: mockLiveServer
+            )
+        )
+>>>>>>> 67a8834 (Initial commit)
         window.makeKeyAndVisible()
     }
 }
